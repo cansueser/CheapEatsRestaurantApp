1daaf233d8be7595c6258db712acc85d7a70d750
cansu <cansue288@gmail.com>
2024-12-09 14:53:41 +0300

    Initial Commit

---
+++ CheapEatsRestaurantApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheapEatsRestaurantApp
+//
+//  Created by CANSU on 9.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
